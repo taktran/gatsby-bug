fa22c642f121cf43991350886d61cbc84ae1682f
Tak Tran <contact@tutaktran.com>
2018-09-26 23:49:38 +0100

    Add test for html and body attributie

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onRenderBody = ({ setBodyAttributes, setHtmlAttributes }) => {
+    setHtmlAttributes({
+      className: 'new-head-class'
+    });
+    setBodyAttributes({
+        className: 'new-body-class'
+      });
+  };
+  
